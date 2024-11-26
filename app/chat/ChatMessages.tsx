450c57a2b1faf17a599f8bb0206b5dfe87c761ef
alexelalex <67682405+alexelalex@users.noreply.github.com>
2024-11-26 03:16:58 +0200

    fix

--- app/chat/ChatMessages.tsx
+++ app/chat/ChatMessages.tsx
@@ -3,21 +3,25 @@
 import { useAppContext } from "@/lib/app-provider";
 
 export function ChatMessages() {
-	const { messages } = useAppContext({
-		subscribeToMessages: true,
-	});
+  const { messages } = useAppContext({
+    subscribeToMessages: true,
+  });
 
-	return (
-		<div>
-			{messages?.map((message, key) => (
-				// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-				<div key={key}>
-					{message.content?.map(({ text }, key) => (
-						// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-						<div key={key}>{text}</div>
-					))}
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div>
+      {messages?.map((message, key) => (
+        // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+        <div key={key}>
+          {(message.content as Content[])?.map(({ text }, key) => (
+            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+            <div key={key}>{text}</div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
+
+type Content = {
+  text: string;
+};
